ee8ae75fd2f256c834c66c6d44c1214b556adac8
Roz Townsend <roslyn.townsend@gmail.com>
2023-07-15 19:25:18 +0200

    first commit

---
+++ forecast.js
@@ -0,0 +1,295 @@
+import axios from "axios";
+
+export function getTimezone(name) {
+    return axios
+    .get("https://geocoding-api.open-meteo.com/v1/search",
+        {params: {
+            name: name,
+            count: 1,
+            format: "json"
+            },
+        })
+    .catch(error => {
+        if (error.response) {
+          // Handle specific HTTP response status codes
+          if (error.response.status === 404) {
+            throw new Error("Can't find the location to get the timezone.");
+          } else {
+            throw new Error("An error occurred during the API request.");
+          }
+        } else {
+          throw new Error("An error occurred while trying to communitcate with the server.");
+        }
+      });
+};
+
+export function getAirQuality(latitude, longitude) {
+    const rawDate = new Date();
+    const paramDates = rawDate.toISOString().split('T')[0];
+    return axios
+        .get("https://air-quality-api.open-meteo.com/v1/air-quality", 
+        {params: {
+            latitude: latitude,
+            longitude: longitude,
+            hourly: "pm2_5,aerosol_optical_depth,alder_pollen,birch_pollen," + 
+            "grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen,european_aqi",
+            timezone: "auto",
+            start_date: paramDates,
+            end_date: paramDates
+            }
+        })
+        .then(({ data }) => {
+            console.log(data)
+            const timezone = data.timezone_abbreviation;
+            const hourlyData = parseAirQuality(data);
+            return {
+                timezone: timezone,
+                hourly: hourlyData
+            };
+        })
+        .catch(error => {
+            console.error(error);
+            throw new Error("There was an error getting the pollen and air quality data.")
+        });
+};
+
+export const pollenRanges = {
+    alder_digits: {
+        low: [0.00, 14.9],
+        medium: [15, 89.9],
+        high: [90, 1499.9],
+        hazardous: [1500, 999999]
+    },
+    birch_digits: {
+        low: [0.00, 14.9],
+        medium: [15, 89.9],
+        high: [90, 1499.9],
+        hazardous: [1500, 999999]
+    },
+    olive_digits: {
+        low: [0.00, 14.9],
+        medium: [15, 89.9],
+        high: [90, 1499.9],
+        hazardous: [1500, 999999]
+    },
+    grass_digits: {
+        low: [0.00, 4.9],
+        medium: [5, 19.9],
+        high: [20, 199.9],
+        hazardous: [200, 999999]
+    },
+    mugwort_digits: {
+        low: [0.00, 9.9],
+        medium: [10, 49.9],
+        high: [50, 499.9],
+        hazardous: [500, 999999]
+    },
+    ragweed_digits: {
+        low: [0.00, 9.9],
+        medium: [10, 49.9],
+        high: [50, 499.9],
+        hazardous: [500, 999999]
+    }
+};
+
+const aqiRanges = {
+    pm_digits: {
+        good: [0.00, 9.9],
+        fair: [10, 19.9],
+        moderate: [20, 24.9],
+        poor: [25, 49.9],
+        very_poor: [50, 74.9],
+        extremely_poor: [75, 999999]
+    },
+    haze_digits: {
+        low: [0.00, 0.29],
+        moderate: [0.30, 0.54],
+        high: [0.55, 0.74],
+        very_high: [0.75, 999999]
+    },
+    aqi_digits: {
+        good: [0.00, 19.9],
+        fair: [20, 39.9],
+        moderate: [40, 59.9],
+        poor: [60, 79.9],
+        very_poor: [80, 99.9],
+        extremely_poor: [100, 999999]
+    }
+};
+
+const labels = {
+    aqiLabels: {
+        pm_label: "Particulate Matter 2.5 (Grains/m³)",
+        haze_label: "Aerosol Optical Depth (Haze)",
+        aqi_label: "European Air Quality Index"
+    },
+    pollenLabels: {
+        alder_label: "Alder",
+        birch_label: "Birch",
+        grass_label: "Grass",
+        mugwort_label: "Mugwort",
+        olive_label: "Olive",
+        ragweed_label: "Ragweed",
+    }
+};
+
+function mapFullDayLevelRanges(readingArraysObj, pollenRanges, index) {
+    let pollenRangesResult = {};
+    for (const key in readingArraysObj) {
+      const value = readingArraysObj[key];
+      const pollenRangeKey = key + "_digits";
+      const newKey = key + "_level";
+      pollenRangesResult[newKey] = [];
+      value.forEach((reading) => {
+        let foundRange = false;
+        for (const pollenRange in pollenRanges[pollenRangeKey]) {
+          const [min, max] = pollenRanges[pollenRangeKey][pollenRange];
+        if (reading >= min && reading <= max) {
+            if (reading === null) {
+                pollenRangesResult[newKey].push("out of season");
+                foundRange = true;
+            } else {
+            pollenRangesResult[newKey].push(pollenRange);
+            foundRange = true;
+          }
+        }
+    }
+    if (!foundRange) {
+        pollenRangesResult[newKey].push("range level not found");
+        }
+    });
+    if (index !== undefined) {
+    pollenRangesResult[newKey] = [pollenRangesResult[newKey][index]]
+        }
+    } 
+    return pollenRangesResult;
+};
+
+function mergeMappedObjects(array) {
+    return array.reduce((result, object) => {
+        for (const key in object) {
+            const prefix = key.split("_")[0];
+            if (!result[prefix]) {
+                result[prefix] = {};
+            }
+            result[prefix][key] = object[key];
+        }
+        return result;
+    }, {});
+}
+
+function parseAirQuality({ hourly }) {
+    const currentTime = new Date().getHours();
+    let {
+        time: hour,
+        pm2_5: particulateMatter,
+        aerosol_optical_depth: aerosolOpticalDepth,
+        european_aqi: europeanAirQuality,
+        alder_pollen: alderIndex,
+        birch_pollen: birchIndex,
+        grass_pollen: grassIndex,
+        mugwort_pollen: mugwortIndex,
+        olive_pollen: oliveIndex,
+        ragweed_pollen: ragweedIndex
+    } = hourly;
+    //timestamp arrays and objs
+    const hours = hour;
+    const hourlyIndex = hours.map(hour => 
+        parseInt(hour.split("T")[1].substring(0,2), 10));
+    const hourlyTimeStamp = hours.map(hour => 
+        hour.split("T")[1].substring(0,5));
+    const currentHoursIndex = hourlyIndex[currentTime];
+    const timestamps = {
+        current_hour: hourlyTimeStamp[currentTime],
+        timestamp: hourlyTimeStamp
+    };
+    
+    //pollen readings for 24 hours
+    const pollenFullDay = {
+        alder: alderIndex,
+        birch: birchIndex,
+        grass: grassIndex,
+        mugwort: mugwortIndex,
+        olive: oliveIndex,
+        ragweed: ragweedIndex};
+    const fullDayPollenLevels = mapFullDayLevelRanges(pollenFullDay, pollenRanges);
+    const fullDayPollenArray = [pollenFullDay, fullDayPollenLevels, labels.pollenLabels];
+    const fullDayPollenReadings = mergeMappedObjects(fullDayPollenArray);
+
+    //current hour's pollen readings
+    const currentPollenDigits = {
+        alder_pollen_digits: alderIndex[currentTime],
+        birch_pollen_digits: birchIndex[currentTime],
+        grass_pollen_digits: grassIndex[currentTime],
+        mugwort_pollen_digits: mugwortIndex[currentTime],
+        olive_pollen_digits: oliveIndex[currentTime],
+        ragweed_pollen_digits: ragweedIndex[currentTime]};
+    const currentHoursPollenLevels = mapFullDayLevelRanges(pollenFullDay, pollenRanges, currentHoursIndex);
+    const currentHoursPollenReadingArray = [currentHoursPollenLevels, currentPollenDigits, labels.pollenLabels];
+    const currentHoursPollensReading = mergeMappedObjects(currentHoursPollenReadingArray);
+    
+    //current hour's aqi data
+    const currentHoursAqiDigits = {
+        pm: particulateMatter[currentTime],
+        haze: aerosolOpticalDepth[currentTime],
+        aqi: europeanAirQuality[currentTime]
+    };
+    const airQualityData = {
+        pm: particulateMatter,
+        haze: aerosolOpticalDepth,
+        aqi: europeanAirQuality
+    };
+
+    //AQI readings for 24 hours
+    const mappedFullDayAqiReadings = mapFullDayLevelRanges(airQualityData, aqiRanges)
+    const fullDayAqiReadingsArray = [mappedFullDayAqiReadings, airQualityData, labels.aqiLabels];
+    const fullDayAqi = mergeMappedObjects(fullDayAqiReadingsArray);
+   
+    //current hours AQI readings
+    const currentHoursAqiLevels = mapFullDayLevelRanges(airQualityData, aqiRanges, currentHoursIndex);
+    const currentHoursAqiReadingsArray = [labels.aqiLabels, currentHoursAqiDigits,  currentHoursAqiLevels]
+    const currentHoursAqiReading = mergeMappedObjects(currentHoursAqiReadingsArray);
+    return {fullDayPollenReadings,
+            currentHoursPollensReading,
+            pollenFullDay,
+            fullDayAqi,
+            currentHoursAqiReading,
+            timestamps
+        }
+    };
+
+export function getDaily(latitude, longitude) {
+    return axios.get("https://api.open-meteo.com/v1/forecast",
+    {params: {
+        latitude: latitude,
+        longitude: longitude,
+        daily: "weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max,winddirection_10m_dominant,precipitation_sum,precipitation_probability_max",
+        windspeed_unit: "ms",
+        timezone: "auto"
+        },
+    }).then(({ data }) => {
+        return {
+            daily: parseDaily(data)}
+    })    
+}
+
+function parseDaily({ daily }) {
+    const {
+        weathercode: [weathercode],
+        temperature_2m_max: [maxTemp], 
+        temperature_2m_min: [minTemp], 
+        precipitation_sum: [precipSum],
+        precipitation_probability_max: [maxPrecip],
+        windspeed_10m_max: [maxWindspeed], 
+        winddirection_10m_dominant: [windDirection]
+    } = daily;
+    return {
+        weathercode: weathercode,
+        highTemp: maxTemp,
+        lowTemp: minTemp,
+        precipProbability: maxPrecip,
+        precipSum: precipSum,
+        windSpeed: maxWindspeed,
+        windDirection: windDirection
+    }
+};
---
+++ script.js
@@ -0,0 +1,383 @@
+import { getTimezone } from "./forecast";
+import { getAirQuality } from "./forecast";
+import { getDaily } from "./forecast";
+
+const inputLocationElement = document.querySelector("[data-location]");
+const displayLocationElement = document.querySelector("[data-return-location]");
+inputLocationElement.value = "Undenäs";
+
+let hourlyData;
+let latitude;
+let longitude;
+let locationName;
+let countryName;
+
+async function fetchData() {
+    try {
+        const locationInput = inputLocationElement.value;
+        const response = await getTimezone(locationInput);
+        console.log(response.data);
+        latitude = response.data.results[0].latitude;
+        longitude = response.data.results[0].longitude;
+        locationName = response.data.results[0].name;
+        countryName = response.data.results[0].country;
+        
+        const airQualityResponse = await getAirQuality(latitude, longitude);
+        const currentTimezone = airQualityResponse.timezone;
+        hourlyData = airQualityResponse.hourly;
+        renderPollenAndAqi(hourlyData, currentTimezone, locationName, countryName);
+        
+        const dailyResponse = await getDaily(latitude, longitude);
+        renderDaily(dailyResponse.daily);
+        
+    } catch (error) {
+        console.error(error);
+        alert("Error occurred during data fetching!");
+    }
+};
+
+// const submitHandler = event => {
+//     event.preventDefault();
+    fetchData();
+// };
+
+
+//set values and code more easily
+function setValues(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value;
+};
+
+function setCode(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).innerHTML = value;
+};
+
+//CSS class additions
+
+const levelColorMapping = {
+    low: "low",
+    fair: "fair",
+    good: "good",
+    medium: "medium",
+    moderate: "moderate",
+    high: "high",
+    poor: "poor",
+    hazardous: "hazardous",
+    extremely_poor: "extremely-poor"
+};
+
+function setLevelColors(selector, readingLevel, levelElement) {
+    let levelColorClass = levelColorMapping[readingLevel];
+    if (levelColorClass) {
+        levelElement.querySelector(`[data-${selector}]`).classList
+            .add(levelColorClass);
+    }
+};
+
+function setPollenBackgroundClass(
+    selector, pollenKey, pollenElement, fullObject) {
+    const allPollenKeys = Object.keys(fullObject);
+    if (pollenKey) {
+        pollenElement.querySelector(`[data-${selector}]`).classList
+            .remove(...allPollenKeys);
+        pollenElement.querySelector(`[data-${selector}]`).classList
+            .add(pollenKey);
+    }
+};
+
+const currentSection = document.querySelector("[data-forecast-cardspread]");
+const pollenCard = document.getElementById("forecast-card");
+const forecastModal = document.querySelector("[data-forecast-modal]");
+const closeModal = document.querySelector("[data-close-modal");
+closeModal.textContent = "X";
+
+//stop body scrolling when the modal is open
+const bodyScrolling = (isOpen) => {
+    if (isOpen) {
+    document.body.classList.add("body-scrolling");
+    } else {
+    document.body.classList.remove("body-scrolling");
+    }
+}
+
+function renderCards(
+    currentHour, 
+    pollenKeys, 
+    hourlyTimestamps, 
+    hourlyPollenData, 
+    currentHourTimestamp) {
+    currentSection.innerHTML = "";
+    let currentHoursReading = currentHour;
+    for (const key of pollenKeys) {
+        const nestedObject = currentHour[key];
+        const nestedKeys = Object.keys(nestedObject)
+        const pollenLevel = nestedObject[nestedKeys[0]];
+        const pollenGrains = nestedObject[nestedKeys[1]];
+        const pollenElement = pollenCard.content.cloneNode(true);
+        setValues("pollen-label", key, { parent: pollenElement });
+        setValues("pollen-level", pollenLevel, { parent: pollenElement });
+        const openForecastModal = pollenElement.querySelector("[data-forecast-link]");
+        if (pollenGrains == null) {
+            setValues("pollen-grains", `0 Grains/m³`, { parent: pollenElement });
+            setValues("forecast-link", "No current forecast", {parent: pollenElement});
+            pollenElement.querySelector("[data-forecast-link]").classList.add("no-link");
+        } else {
+            openForecastModal.addEventListener("click", function(event) {
+                event.preventDefault();
+                const keyArray = [key];
+                renderReadingsTable(hourlyTimestamps, hourlyPollenData, keyArray, currentHourTimestamp);
+                let isOpen = true;
+                bodyScrolling(isOpen);
+                forecastModal.showModal();
+            });
+            setValues("pollen-grains",
+                `${pollenGrains} Grains/m³`,
+                { parent: pollenElement });
+            setCode("forecast-link",
+                `<a class="light-link" href="#" data-modal="full-day-${key}">See full-day forecast</a>`,
+                { parent: pollenElement });
+        }
+        setLevelColors("pollen-level", pollenLevel, pollenElement);
+        setPollenBackgroundClass("forecast-card", key, pollenElement, currentHoursReading)
+        currentSection.append(pollenElement);
+    } 
+};
+
+const aqiSection = document.querySelector("[data-aqi-spread]");
+const aqiDatapoint = document.getElementById("aqi-datapoint");
+
+function renderCurrentHoursAqiData(aqiData, aqiKeys) {
+    aqiSection.innerHTML = "";
+    for (const key of aqiKeys) {
+        const nestedObject = aqiData[key];
+        const nestedKeys = Object.keys(nestedObject);
+        const aqiLabels = nestedObject[nestedKeys[0]];
+        const aqiDigits = nestedObject[nestedKeys[1]];
+        const aqiLevel = nestedObject[nestedKeys[2]];
+        const aqiElement = aqiDatapoint.content.cloneNode(true);
+        setValues("aqi-label", aqiLabels, { parent: aqiElement });
+        setValues("aqi-level", aqiLevel, { parent: aqiElement });
+        setValues("aqi-digits", aqiDigits, { parent: aqiElement });
+        setLevelColors("aqi-level", aqiLevel, aqiElement);
+        aqiSection.append(aqiElement);
+    }
+};
+
+//modal rendering and tables
+const tableHeadersSection = document.querySelector("[data-table-headers]");
+const tableHeader = document.getElementById("hourly-table-headers");
+
+function renderTableHeaders(fullDayObject, objectKey) {
+    let gatheredHeaders = [];
+    
+    if (objectKey.length === 1) {
+    gatheredHeaders = ["Hours", "Level", "Grains/m³"];
+    gatheredHeaders.forEach(label => {
+        const tableHeaderElement = tableHeader.content.cloneNode(true);
+        setValues("header-label", label, { parent: tableHeaderElement })
+        tableHeadersSection.append(tableHeaderElement);
+        });
+        return;
+    } else {
+        for (objectKey in fullDayObject) {
+            const headerLabel = fullDayObject[objectKey][`${objectKey}_label`];
+            gatheredHeaders.push(headerLabel);
+        }
+        gatheredHeaders.unshift("Hours");
+        gatheredHeaders.forEach(label => {
+            const tableHeaderElement = tableHeader.content.cloneNode(true);
+            setValues("header-label", label, { parent: tableHeaderElement })
+            tableHeadersSection.append(tableHeaderElement);
+            });
+    }
+};
+
+const modalSection = document.querySelector("[data-modal-section]");
+const hourlySection = document.querySelector("[data-hourly-table]");
+const hourlyRow = document.getElementById("hourly-table-row");
+const hourlyHeader = document.querySelector("[data-pollen-label]");
+const currentDateRaw = new Date();
+const currentDateFormatted = currentDateRaw.toLocaleString([], 
+    {day: "numeric", weekday: "long", month: "long"});
+
+    
+closeModal.addEventListener("click", () => {
+    tableHeadersSection.innerHTML = "";
+    hourlySection.innerHTML = "";
+    let isOpen = false;
+    bodyScrolling(isOpen);
+    forecastModal.close();
+});
+
+function outOfSeasonFilter(fullDayObject, objectKey) {
+    //remove out-of-season pollens from fullDayObject in table columns
+    if (objectKey.length === 1) {
+        return objectKey;
+    } else {
+    let filteredArray;
+    console.log(objectKey);
+        for (objectKey in fullDayObject) {
+            console.log(objectKey)
+            const readingArray = fullDayObject[objectKey][objectKey];
+            if (readingArray.every(item => item === null)) {
+                delete fullDayObject[objectKey];
+                console.log(`${objectKey} is out of season!`)
+            }
+        }
+        filteredArray = Object.keys(fullDayObject);
+        return filteredArray;
+        }
+    }
+    
+function renderReadingsTable(
+    hourlyTimestamps, 
+    fullDayObject, 
+    objectKey, 
+    currentHourTimestamp) {
+    const filteredObjectKey = outOfSeasonFilter(fullDayObject, objectKey);
+    renderTableHeaders(fullDayObject, filteredObjectKey);
+    const minLength = hourlyTimestamps.length;
+    const objectLength = filteredObjectKey.length;        
+    let formattedArray = Array(objectLength);
+    let currentIndex = 0;
+    for (let i = 0;
+        i < minLength;
+        i++) {
+            let rowLevel;
+            let rowReading;
+            const rowTimestamp = hourlyTimestamps[i];
+            let rowTimestampTd = `<td>${rowTimestamp}</td>`;
+            let formattedTd = "";
+            let fullRowTds;
+            let levelColorClass;
+            const hourlyTableElement = hourlyRow.content.cloneNode(true);
+            if (objectLength === 1) {
+                setPollenBackgroundClass(
+                    "forecast-modal", objectKey, modalSection, fullDayObject);
+                rowLevel = fullDayObject[objectKey][`${objectKey}_level`][i];
+                rowReading = fullDayObject[objectKey][objectKey][i];
+                hourlyHeader.textContent = `${objectKey} forecast: ${currentDateFormatted}`;
+                levelColorClass = levelColorMapping[rowLevel];
+                formattedTd =
+                `
+                <td class="${levelColorClass}">${rowLevel}</td>
+                <td>${rowReading}</td>
+                `;
+            } else if (objectLength > 1) {
+                for (objectKey in fullDayObject) {
+                    hourlyHeader.textContent = `Full day forecast: ${currentDateFormatted}`;
+                    rowLevel = fullDayObject[objectKey][`${objectKey}_level`][i];
+                    rowReading = fullDayObject[objectKey][objectKey][i];
+                    levelColorClass = levelColorMapping[rowLevel];
+                    formattedTd += `
+                        <td><span class="${levelColorClass}">${rowLevel}</span>
+                        <br>(${rowReading})</td>
+                        `;
+                        currentIndex++;
+                        }
+                    }
+                    formattedArray[currentIndex] = formattedTd.trim();
+                    currentIndex++;
+                        const subsequentDataPoints = 
+                        formattedArray.slice(currentIndex - objectLength, currentIndex);
+                        const cleanedDataPoints = subsequentDataPoints.map((td) => td.trim())
+                        fullRowTds = rowTimestampTd + cleanedDataPoints.join("");
+                        setCode("table-row", fullRowTds, { parent: hourlyTableElement });
+            if (rowTimestamp == currentHourTimestamp) {
+                hourlyTableElement.querySelector("[data-table-row]").classList
+                .add("hourly-table__row-current");
+            };
+            hourlySection.append(hourlyTableElement);
+            formattedArray = Array(objectLength);
+    }
+};
+        
+const fullPollensModalLink = document.querySelector("[data-fullpollen-modal]");
+const aqiModalLink = document.querySelector("[data-aqi-modal]");
+        
+function renderPollenAndAqi(hourlyData, currentTimezone, locationName, countryName) {
+    const {
+        timestamps: {current_hour: currentHourTimestamp, timestamp: hourlyTimestamps},
+        currentHoursPollensReading: currentHour,
+        fullDayPollenReadings: hourlyPollenData,
+        currentHoursAqiReading: currentAqiData,
+        fullDayAqi: aqiFullDay
+    } = hourlyData;
+
+    //render headers
+    const rawTime = new Date();
+    const asOfTimestamp = `${rawTime.toLocaleTimeString([], 
+        { hour: "2-digit", minute: "2-digit" })}`;
+    displayLocationElement.textContent = 
+    `Why you're sneezing in ${locationName}, ${countryName}`
+    document.querySelector("[data-as-of-time]").textContent = 
+    `As of ${asOfTimestamp} ${currentTimezone}`;
+    const pollenKeys = Object.keys(currentHour);
+
+    //render current hour's pollen cards
+    renderCards(
+        currentHour, pollenKeys, hourlyTimestamps, hourlyPollenData, currentHourTimestamp);
+    fullPollensModalLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        renderReadingsTable(
+            hourlyTimestamps, hourlyPollenData, pollenKeys, currentHourTimestamp);
+        let isOpen = true;
+        bodyScrolling(isOpen);
+        forecastModal.showModal();}
+    );
+
+    //render current hours's AQI
+    const aqiKeys = Object.keys(currentAqiData);
+    renderCurrentHoursAqiData(currentAqiData, aqiKeys);
+    aqiModalLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        renderReadingsTable(
+            hourlyTimestamps, aqiFullDay, aqiKeys, currentHourTimestamp);
+        let isOpen = true;
+        bodyScrolling(isOpen);
+        forecastModal.showModal();}
+    );
+};
+
+//Render today's weather
+function renderDaily({ 
+    highTemp, 
+    lowTemp, 
+    precipProbability, 
+    precipSum, 
+    windSpeed, 
+    windDirection }) {
+        console.log(highTemp)
+    const windDegrees = windDirection;
+    setValues("high-temp", highTemp, document);
+    setValues("low-temp", lowTemp, document);
+    setValues("precipitation-probability", precipProbability, document);
+    setValues("precipitation-sum", precipSum, document);
+    setValues("max-windspeed", windSpeed, document);
+    setValues("wind-direction", windDegrees, document);
+    setValues("wind-direction-name", getWindDirection(windDegrees), document);
+};
+
+function getWindDirection(windDegrees) {
+    switch (true) {
+    case windDegrees >= 337.5 || windDegrees < 22.5:
+        return 'North';
+    case windDegrees >= 22.5 && windDegrees < 67.5:
+        return 'Northeast';
+    case windDegrees >= 67.5 && windDegrees < 112.5:
+        return 'East';
+    case windDegrees >= 112.5 && windDegrees < 157.5:
+        return 'Southeast';
+    case windDegrees >= 157.5 && windDegrees < 202.5:
+        return 'South';
+    case windDegrees >= 202.5 && windDegrees < 247.5:
+        return 'Southwest';
+    case windDegrees >= 247.5 && windDegrees < 292.5:
+        return 'West';
+    case windDegrees >= 292.5 && windDegrees < 337.5:
+        return 'Northwest';
+    default:
+        return 'Degree value is invalid.';
+    };
+};
+
+    // formElement.addEventListener("submit", submitHandler);
